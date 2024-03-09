e80fc07dbcb59e5a4277e0fa76751d4f49b6d652
sgtjlanc <jamowaind@gmail.com>
2024-03-09 22:32:32 +0000

    Sustainability

---
+++ src/ii_integration_frontend/src/plugin/sustainability.ts
@@ -0,0 +1,18 @@
+import { plugin } from "./plugin.ts";
+
+const SustainabilityPlugin: plugin = {
+    name: "Company Sustainability Reporter",
+    description: "A plugin to support reporting sustainability levels for the company based off of carbon footprint, resource efficiency, ethical standards and coorperate governance.",
+    version: "0.1.0",
+    fact_id: "sustainability",
+    functions: [
+        {
+            name: "ReportSustainability",
+            description: "Input sustainibility facts and calculate the company's total sustainability levels",
+            accessible_facts: ["sustainability"],
+            prompt_transformer: "Company Sustainability Facts: $$facts$$. Add these factors up and divide the total by the total number of factors provided to get the total sustainability score. Return this score"
+        
+        }
+    ]
+}
+export default SustainabilityPlugin;
